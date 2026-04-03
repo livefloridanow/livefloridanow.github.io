4ef021a69a4d39186a6308352bcc0cf5dbab22ac
Owen Dupree <owendupree@Owens-MacBook-Air.local>
2026-04-03 11:17:23 -0400

    Tighten logo SVG viewBox

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -23,7 +23,7 @@ export default function Logo({ variant = 'dark', className }: LogoProps) {
 
   return (
     <svg
-      viewBox="0 0 230 40"
+      viewBox="0 0 200 40"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
